959e5a5afecdb3ab7842ebb3f8a6eaa88d86fddd
@小小只……^_^ <littlezov@qq.com>
2022-06-20 09:59:57 +0800

    builder

--- builds.sh
+++ builds.sh
@@ -14,31 +14,34 @@ for version; do
 	fullVersion="$(jq -r '.[env.version].version' versions.json)"
 	eval "variants=( $variants )"
 	eval "fullVersion=( $fullVersion )"
-	# echo $version
 	for dir in "${variants[@]}"; do
 		suite="$(dirname "$dir")"    # "buster", etc
 		variant="$(basename "$dir")" # "cli", etc
 		alpineVer="${suite#alpine}"  # "3.12", etc
-		# echo ''
 		tags="littleof/php:$fullVersion-$variant-$suite"
 		shortTags="littleof/php:$fullVersion-$variant"
 		versionTags="littleof/php:$fullVersion"
 		shortversionTags="littleof/php:$fullVersion"
-		# echo ''
-		echo build $versionTags
+		echo build $tags
 		docker build -t $tags -f "$version/$dir/"Dockerfile ./"$version/$dir/"
 		docker push $tags
-		docker tag $tags $shortTags
-		docker push $shortTags
-		docker tag $tags $versionTags
-		docker push $versionTags
-		docker tag $tags littleof/php:$version
-		docker push littleof/php:$version
-		if [[ "$version" == "7.4" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "buster" ]]; then
+		if [[ "$version" == "7.4" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "bullseye" ]]; then
+			docker tag $tags $shortTags
+			docker push $shortTags
+			docker tag $tags $versionTags
+			docker push $versionTags
+			docker tag $tags littleof/php:$version
+			docker push littleof/php:$version
 			docker tag $tags littleof/php:7
 			docker push littleof/php:7
 		fi
-		if [[ "$version" == "8.1" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "buster" ]]; then
+		if [[ "$version" == "8.1" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "bullseye" ]]; then
+			docker tag $tags $shortTags
+			docker push $shortTags
+			docker tag $tags $versionTags
+			docker push $versionTags
+			docker tag $tags littleof/php:$version
+			docker push littleof/php:$version
 			docker tag $tags littleof/php:8
 			docker push littleof/php:8
 			docker tag $tags littleof/php:latest
