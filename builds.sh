2549d2b2de826c6af464e9b17222ea04d64001d5
@小小只^v^ <littlezov@qq.com>
2022-12-27 20:03:43 +0800

    up

--- builds.sh
+++ builds.sh
@@ -1,57 +1,104 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-[ -f versions.json ] # run "versions.sh" first
+declare -A aliases=(
+	[8.2]='8 latest'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 if [ "$#" -eq 0 ]; then
-	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	versions="$(jq -r 'to_entries | map(if .value then .key | @sh else empty end) | join(" ")' versions.json)"
 	eval "set -- $versions"
 fi
 
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
 for version; do
-	export version
+	rcVersion="${version%-rc}"
+	export version rcVersion
+
+	if ! fullVersion="$(jq -er '.[env.version] | if . then .version else empty end' versions.json)"; then
+		continue
+	fi
+
+	if [ "$rcVersion" != "$version" ] && rcFullVersion="$(jq -er '.[env.rcVersion] | if . then .version else empty end' versions.json)"; then
+		# if this is a "-rc" release, let's make sure the release it contains isn't already GA (and thus something we should not publish anymore)
+		latestVersion="$({ echo "$fullVersion"; echo "$rcFullVersion"; } | sort -V | tail -1)"
+		if [[ "$fullVersion" == "$rcFullVersion"* ]] || [ "$latestVersion" = "$rcFullVersion" ]; then
+			# "x.y.z-rc1" == x.y.z*
+			continue
+		fi
+	fi
+
 	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
-	fullVersion="$(jq -r '.[env.version].version' versions.json)"
 	eval "variants=( $variants )"
-	eval "fullVersion=( $fullVersion )"
+
+	versionAliases=(
+		$fullVersion
+		$version
+		${aliases[$version]:-}
+	)
+
+	defaultDebianVariant="$(jq -r '
+		.[env.version].variants
+		| map(
+			split("/")[0]
+			| select(
+				startswith("alpine")
+				| not
+			)
+		)
+		| .[0]
+	' versions.json)"
+	defaultAlpineVariant="$(jq -r '
+		.[env.version].variants
+		| map(
+			split("/")[0]
+			| select(
+				startswith("alpine")
+			)
+		)
+		| .[0]
+	' versions.json)"
+
 	for dir in "${variants[@]}"; do
-		suite="$(dirname "$dir")"    # "buster", etc
+		suite="$(dirname "$dir")" # "buster", etc
 		variant="$(basename "$dir")" # "cli", etc
-		alpineVer="${suite#alpine}"  # "3.12", etc
-		tags="littleof/php:$fullVersion-$variant-$suite"
-		shortTags="littleof/php:$fullVersion-$variant"
-		versionTags="littleof/php:$fullVersion"
-		shortversionTags="littleof/php:$fullVersion"
-		echo build $tags
-		docker build -t $tags -f "$version/$dir/"Dockerfile ./"$version/$dir/"
-		docker push $tags
-		if [[ "$version" == "7.4" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "bullseye" ]]; then
-			docker tag $tags $shortTags
-			docker push $shortTags
-			docker tag $tags $versionTags
-			docker push $versionTags
-			docker tag $tags littleof/php:$version
-			docker push littleof/php:$version
-			docker tag $tags littleof/php:7
-			docker push littleof/php:7
+		dir="$version/$dir"
+		[ -f "$dir/Dockerfile" ] || continue
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		if [ "$variant" = 'cli' ]; then
+			variantAliases+=( "${versionAliases[@]}" )
 		fi
-		if [[ "$variant" == "cli" ]] && [[ "$suite" == "bullseye" ]]; then
-			docker tag $tags $shortTags
-			docker push $shortTags
-			docker push $versionTags-$variant
-			docker tag $tags littleof/php:$version-$variant
+
+		suiteVariantAliases=( "${variantAliases[@]/%/-$suite}" )
+		if [ "$suite" = "$defaultAlpineVariant" ] ; then
+			variantAliases=( "${variantAliases[@]/%/-alpine}" )
+		elif [ "$suite" != "$defaultDebianVariant" ]; then
+			variantAliases=()
 		fi
-		if [[ "$version" == "8.2" ]] && [[ "$variant" == "cli" ]] && [[ "$suite" == "bullseye" ]]; then
-			docker tag $tags $shortTags
-			docker push $shortTags
-			docker tag $tags $versionTags
-			docker push $versionTags
-			docker tag $tags littleof/php:$version
-			docker push littleof/php:$version
-			docker tag $tags littleof/php:8
-			docker push littleof/php:8
-			docker tag $tags littleof/php:latest
-			docker push littleof/php:latest
-		fi
+		variantAliases=( "${suiteVariantAliases[@]}" ${variantAliases[@]+"${variantAliases[@]}"} )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+		# variantArches="${parentRepoToArches[$variantParent]}"
+		for ver in ${variantAliases[@]}; do
+		{
+			echo build "littleof/php:$ver"
+			docker build -t "littleof/php:$ver" -f "$dir/"Dockerfile ./"$dir"
+			docker push "littleof/php:$ver"
+		}&
+		done
+		# echo  $(join ', ' "${variantAliases[@]}")
+		wait
 	done
 done
