e5f7bd31830f18abe57be2a13626129a9e1c5c15
@小小只^v^ <littlezov@qq.com>
2023-06-19 14:46:00 +0800

    up

--- builds.sh
+++ builds.sh
@@ -176,13 +176,14 @@ for version; do
 		# variantArches="${parentRepoToArches[$variantParent]}"
 		for ver in ${variantAliases[@]}; do
 		{
-			echo build "littleof/php:$ver"
-			docker build -t "littleof/php:$ver" -f "$dir/"Dockerfile ./"$dir"
-			docker push "littleof/php:$ver"
-		}&
+			echo build "$NAMESOACE/php:$ver"
+			docker build -t "$NAMESOACE/php:$ver" -f "$dir/"Dockerfile ./"$dir"
+			docker push "$NAMESOACE/php:$ver"
+		}
 		done
 		# echo  $(join ', ' "${variantAliases[@]}")
 		wait
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -197,6 +198,9 @@ for version; do
 =======
 	}&
 >>>>>>> da7cf35b (up)
+=======
+	}
+>>>>>>> cf9523ad (up)
 	done
 	wait
 }
