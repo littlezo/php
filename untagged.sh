e094b0488c50ef093afdd46ad96a0544b30ca723
@小小只^v^ <littlezov@qq.com>
2024-05-26 15:34:26 +0800

    up

--- untagged.sh
+++ untagged.sh
@@ -4,10 +4,11 @@ OWNER="littlezo"
 REPO="php"
 PACKAGE_NAME="php"
 TOKEN= ${{ secrets.GITHUB_TOKEN }}
-
+# TOKEN="$GITHUB_TOKEN"
+# echo $TOKEN
 # 获取所有未标记的版本
 untagged_versions=$(curl -H "Authorization: bearer $TOKEN" -H "Accept: application/vnd.github.v3+json" \
-"https://api.github.com/user/packages/container/php/versions?per_page=100" | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id')
+"https://api.github.com/user/packages/container/php/versions?per_page=200" | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id')
 
 #  | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id')
 echo $untagged_versions
