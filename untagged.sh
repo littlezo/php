cb1006da2d1ce72f1ccb5b68e2a9061382bdec48
@小小只^v^ <littlezov@qq.com>
2024-07-21 11:38:13 +0800

    ci

--- untagged.sh
+++ untagged.sh
@@ -3,22 +3,37 @@
 OWNER="littlezo"
 REPO="php"
 PACKAGE_NAME="php"
-TOKEN= ${{ secrets.GITHUB_TOKEN }}
-# TOKEN="$GITHUB_TOKEN"
-# echo $TOKEN
+TOKEN="$GITHUB_TOKEN"
 # 获取所有未标记的版本
 untagged_versions=$(curl -H "Authorization: bearer $TOKEN" -H "Accept: application/vnd.github.v3+json" \
-"https://api.github.com/user/packages/container/php/versions?per_page=200" | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id')
+"https://api.github.com/user/packages/container/php/versions?per_page=200")
 
-#  | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id')
-echo $untagged_versions
-# 循环删除每个未标记的版本
-for version in $untagged_versions
-do
-{
-  echo "Deleting version $version"
-  curl -X DELETE -H "Authorization: bearer $TOKEN" -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/user/packages/container/php/versions/$version"
-}&
-done
-wait
+fctch(){
+  echo $(curl -H "Authorization: bearer $TOKEN" -H "Accept: application/vnd.github.v3+json" \
+"https://api.github.com/user/packages/container/php/versions?per_page=200")
+}
+
+remove() {
+  untagged_versions=$1
+  # 循环删除每个未标记的版本
+  for version in $untagged_versions
+  do
+  {
+    echo "Deleting version $version"
+    curl -X DELETE -H "Authorization: bearer $TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/user/packages/container/php/versions/$version" -o /dev/null
+  }&
+  done
+  wait
+}
+test(){
+  result=$(fctch)
+  if [ "$((echo $result) | jq -r '. | type')" == "array" ]; then
+      untagged_versions=($((echo $result) | jq -r '.[] | select(.metadata.container.tags | length == 0) | .id'))
+      if [ "${#untagged_versions[@]}" -gt 0 ]; then
+        remove $untagged_versions
+        test
+      fi
+  fi
+}
+test
